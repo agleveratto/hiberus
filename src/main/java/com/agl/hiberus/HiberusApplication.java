19d41dd139e947a11d974fdca3989f0e73e6b8bb
agleveratto <agleveratto@gmail.com>
2023-05-16 08:45:34 -0300

    feat: added pom, h2 database with data

---
+++ src/main/java/com/agl/hiberus/HiberusApplication.java
@@ -0,0 +1,11 @@
+package com.agl.hiberus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiberusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HiberusApplication.class, args);
+    }
+}
